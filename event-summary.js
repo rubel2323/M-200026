95f85e5150dcea4a2eef9b73d7bc29892ba1d403
Rubel <mr.adcadre41@gmail.com>
2026-03-08 10:59:44 +0600

    update button added with displaying text

---
+++ event-summary.js
@@ -0,0 +1,10 @@
+function Display() {
+  const btn = document.getElementById("input-text");
+
+  btn.innerText = "Have a Nice day!";
+}
+
+function show() {
+  document.getElementById("input-text").innerText =
+    "Input Text is Showing anonymously";
+}
