5d9eab40e98f69954e286ecc5bd3f255c9396486
Rubel <mr.adcadre41@gmail.com>
2026-03-08 12:05:01 +0600

    message displayed

--- event-summary.js
+++ event-summary.js
@@ -8,3 +8,13 @@ function show() {
   document.getElementById("input-text").innerText =
     "Input Text is Showing anonymously";
 }
+
+const btn = document.getElementById("update-btn");
+btn.addEventListener("click", function () {
+  const input = document.getElementById("input-field");
+  const inputTextDisplay = input.value;
+
+  const h2tag = document.getElementById("input-text");
+  h2tag.innerText = inputTextDisplay;
+  input.value = "";
+});
